e3c58d9b06b05fb0789c8dc9f64bde3d2e3c5ebb
hashimotz07 <lucasissamuhashimoto@gmail.com>
2025-07-12 16:40:27 -0300

    feat: agora salva e le as chaves

--- main.py
+++ main.py
@@ -1,5 +1,14 @@
 from keygen import gerador_chaves_rsa
+from utils import serialize_key, save_key_to_pem
 
 public, private = gerador_chaves_rsa(bits=2048)
 print("Chave pública (e, n):", public)
 print("Chave privada (d, n):", private)
+
+public_data = serialize_key(*public)
+private_data = serialize_key(*private)
+
+save_key_to_pem("public_key.pem", public_data, key_type="public")
+save_key_to_pem("private_key.pem", private_data, key_type="private")
+
+print("Chaves salvas com sucesso!")
